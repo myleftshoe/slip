00358d8ae973916bbc23bfe034e1bdd681429bfc
Paul <paulesammut@gmail.com>
2018-10-26 12:40:06 +1100

    Updates

---
+++ precache-manifest.738cd6d25a04c2f103d144ac72b898f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8e0f8618bac0368d665",
+    "url": "/slip/static/js/runtime~main.d8e0f861.js"
+  },
+  {
+    "revision": "cf9a4e1cb6cdd690b2c2",
+    "url": "/slip/static/js/main.cf9a4e1c.chunk.js"
+  },
+  {
+    "revision": "443ca30c2799b135bb08",
+    "url": "/slip/static/js/1.443ca30c.chunk.js"
+  },
+  {
+    "revision": "cf9a4e1cb6cdd690b2c2",
+    "url": "/slip/static/css/main.74687e05.chunk.css"
+  },
+  {
+    "revision": "ae25caaf47793ece83476e5c8a4d52e8",
+    "url": "/slip/index.html"
+  }
+];
