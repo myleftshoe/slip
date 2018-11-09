92108fec64bd67e75ccb7d8bfb425f7f5de20043
Paul <paulesammut@gmail.com>
2018-11-09 21:30:01 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/slip/precache-manifest.2ca9719c8a1d96b68bd17ba8526daa17.js"
+  "/slip/precache-manifest.7b480bfbd7955d27886d4390b0df8321.js"
 );
 
 workbox.clientsClaim();
